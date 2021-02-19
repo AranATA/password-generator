8df7bbfab9edc678f2ef7852dda274e86f1f7ef3
Ghibli <aran.t.ali@gmail.com>
2021-02-18 20:58:55 -0500

    script file was modified, it can write to screen now.

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,13 @@
 // Assignment Code
 
-// all variables are here to stay at global scope
 var generateBtn = document.querySelector("#generate");
 
-// this variable below "trial" is just for checking if the document.getElementById at the end works or not.
-var trial = finalCharacters = '';
+var passLength;
+var userLower;
+var userUpper;
+var userNumber;
+var userSpecial;
 
-// these 5 are the variables are created by user inputs.
-var passLength
-var userLower
-var userUpper
-var userNumber
-var userSpecial
-
-
-// these are the arrays to be defined by prompts and be used in the function.
 var lowerCharacter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var upperCharacter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -23,13 +16,12 @@ var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 var special = [']', ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\', ^', '_', '`', '{', '|', '}', '~'];
 
-// contents of this variable is supposedly created by the function generatePassword after it receives the inputs.
 var finalCharacters = '';
-
+var mes;
 
 function generatePassword() {
   
-// all array combinations are defined below, except ALL FALSE - when I tried to include it; that code sometimes brought up the prompt window again if nothing is picked but caused "maximum call stack size exceeded" error message appear, I guess due to infinite loops, at other times so I skipped it for the time being.
+// all array combinations are defined below.
 
   var array = [];
  
@@ -80,36 +72,38 @@ function generatePassword() {
     finalCharacters += getRandom();
   }
   
-  // this return line is required to bring the result on the console but I am yet to learn the reason behind it, I am confused here.
   return finalCharacters;
- 
-}
+ }
 
-// I simplified this section here and if you write a string instead of the variable trial you can see that it works.
-document.getElementById("password").innerHTML = trial;
+generateBtn.addEventListener("click", writePassword);
 
-
-// Add event listener to generate button
-
-generateBtn.addEventListener("click", wPassword);
-
-function wPassword() {
-   
+function writePassword() {
+  
   passLengthStr = prompt("Please enter the number of characters you want for your new password. It must be more than 12 and less than 128.");
   passLength = Number(passLengthStr);
-
-  userLower = confirm("Do you want lowercase letters in your password?");
-
-  userUpper = confirm("Do you want uppercase letters in your password?");
-
-  userNumber = confirm("Do you want numbers in your password?");
-
-  userSpecial = confirm("Do you want special characters in your password?");
-
-  // if (userLower === false && userUpper === false && userNumber === false && userSpecial === false) {return wPassword();
-
-  console.log(generatePassword());
-
+  if (passLengthStr<12||passLengthStr>128){
+    return writePassword();
+  } 
   
-  // I am yet to provide a limiting code for the passLength variable so that it stays between 12 and 128. Thanks again!
+  userLower = confirm("Do you want lowercase letters in your password?");
+  userUpper = confirm("Do you want uppercase letters in your password?");
+  userNumber = confirm("Do you want numbers in your password?");
+  userSpecial = confirm("Do you want special characters in your password?");
+  if (userLower === false && userUpper === false && userNumber === false && userSpecial === false) {
+    return writePassword();
+  }
+  
+  console.log(generatePassword());
+  
+  var passwordText = document.querySelector("#password");
+  passwordText.value = finalCharacters;
+     
 }
+
+generateBtn.addEventListener("mouseover", location.reload());
+
+
+
+
+ 
+   
